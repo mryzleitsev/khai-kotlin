3ed49fc1214802b85a1ac4ed136f5f37761a0053
X1atle <myron.ryzleitsev@gmail.com>
2023-09-08 18:27:06 +0200

    First lab added

--- FirstLab/app/src/main/java/com/khai/firstlab/MainActivity.kt
+++ FirstLab/app/src/main/java/com/khai/firstlab/MainActivity.kt
@@ -2,10 +2,22 @@ package com.khai.firstlab
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import androidx.core.widget.doOnTextChanged
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initUI()
+    }
+    private fun initUI(){
+        val editText = findViewById<EditText>(R.id.editTextTextPersonName)
+        val textView = findViewById<TextView>(R.id.textView)
+
+        editText.doOnTextChanged { text, start, before, count ->
+            textView.text = text
+        }
     }
 }
